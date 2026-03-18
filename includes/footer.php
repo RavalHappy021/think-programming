5615e196123949130311a1d2d1609022b7528cf5
RavalHappy021 <happyraval09@gmail.com>
2026-03-18 12:30:55 +0530

    Remove YouTube and X links from footer

--- includes/footer.php
+++ includes/footer.php
@@ -9,8 +9,6 @@
         <div class="social-links">
             <a href="mailto:contact@thinkprogramming.com" title="Email Us"><i class="fas fa-envelope"></i></a>
             <a href="https://linkedin.com/" target="_blank" title="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
-            <a href="https://youtube.com/" target="_blank" title="YouTube"><i class="fab fa-youtube"></i></a>
-            <a href="https://x.com/" target="_blank" title="Follow us on X"><i class="fab fa-x-twitter"></i></a>
             <a href="https://instagram.com/" target="_blank" title="Instagram"><i class="fab fa-instagram"></i></a>
         </div>
         <p class="copyright">&copy; <?= date('Y') ?> Think Programming. All rights reserved.</p>
